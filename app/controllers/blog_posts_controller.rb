c81e3c9f7f52243c84160e494c21638c1bd57097
TIMM SNOW <two.tone.snow@gmail.com>
2021-08-12 19:36:35 -0400

    completes rspec tutorial

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -7,12 +7,12 @@ class BlogPostsController < ApplicationController
   end
 
   def create
-        @blog_post = BlogPost.new(blog_post_params)
+    @blog_post = BlogPost.create(blog_post_params)
   end
 
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title)
+   params.require(:blog_post).permit(:title, :body)
   end
 end
