c5f2de8f194c7de35b1463e0e3a8b72f47a235fd
TIMM SNOW <two.tone.snow@gmail.com>
2021-08-12 19:33:56 -0400

    adds create action and view, and private action to permit the allowed params

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,4 +5,14 @@ class BlogPostsController < ApplicationController
    @blog_post = BlogPost.new
 
   end
+
+  def create
+        @blog_post = BlogPost.new(blog_post_params)
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title)
+  end
 end
